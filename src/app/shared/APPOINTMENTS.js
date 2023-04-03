50307f0bf4cd1e8f4447faa4914ae6af185ff907
Miguel Lopez <mlopezt.1215@gmail.com>
2023-04-02 18:15:33 -0700

    APPOINTMENT objects and start files for appointment page

---
+++ src/app/shared/APPOINTMENTS.js
@@ -0,0 +1,42 @@
+export const APPOINTMENTS = [
+    {
+        id: 0,
+        type: 'Doctor',
+        name: 'Dr. Peterson',
+        title: 'Physical',
+        description: 'Yearly Physical with Dr. Peterson.',
+        date: '2023-4-25T11:00Z'
+    },
+    {
+        id: 1,
+        type: 'Dentist',
+        name: 'Dr. Dino',
+        title: 'Cleaning',
+        description: 'Teeth Cleaning with new dentist, Dr. Dino.',
+        date: '2023-5-15T13:15Z'
+    },
+    {
+        id: 2,
+        type: 'Mechanic',
+        name: 'Oil Shop',
+        title: 'Oil Change',
+        description: '25,000 ml oil change.',
+        date: '2023-4-20T14:30Z'
+    },
+    {
+        id: 3,
+        type: 'Education',
+        name: 'Mr. Sanchez',
+        title: 'Spanish Tutor',
+        description: 'Advanced spanish lessons with Mr. Sanchez.',
+        date: '2023-4-18T18:30Z'
+    },
+    {
+        id: 4,
+        type: 'Psychiatrist',
+        name: 'Mrs. Riem',
+        title: 'Monthly Checkup',
+        description: 'Montly checkup with Mrs. Riem for mental health.',
+        date: '2023-5-10T16:00Z'
+    }
+];
